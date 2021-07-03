c6c90aa2a85cebc552c49ab82b5470d52ca3a44b
hanzehua <GW00228842@TECH.GWM.COM>
2021-07-03 10:29:27 +0800

    modify map and build

--- extension/bmap/BMapCoordSys.js
+++ extension/bmap/BMapCoordSys.js
@@ -205,7 +205,9 @@ BMapCoordSys.create = function (ecModel, api) {
 
       bmapRoot.classList.add('ec-extension-bmap');
       root.appendChild(bmapRoot);
-      var bmap = bmapModel.__bmap = new BMap.Map(bmapRoot);
+      var bmap = bmapModel.__bmap = new BMap.Map(bmapRoot, {
+        enableMapClick: false
+      });
       var overlay = new Overlay(viewportRoot);
       bmap.addOverlay(overlay); // Override
 
